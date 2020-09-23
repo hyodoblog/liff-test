b545d385c9587b021e0cc0a2173d80974fc2a0c1
yukihira27sao <yusei.hyodo@gmail.com>
2020-09-24 03:14:35 +0900

    初期化

---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  srcDir: 'src/',
+  ssr: false,
+  head: {
+    titleTemplate: '%s - liff-form',
+    title: 'liff-form',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/vuetify
+    '@nuxtjs/vuetify',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [],
+
+  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+}
